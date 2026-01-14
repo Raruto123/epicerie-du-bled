2af596841585d787460ea903c2efc1ee9eb9c01a
mohamedsane <sanem9206@gmail.com>
2026-01-13 22:26:16 -0500

    test

--- services/onboardingService.js
+++ services/onboardingService.js
@@ -3,13 +3,13 @@ import { STORAGE_KEYS } from "../constants/storageKeys";
 
 export async function getHasSeenOnboarding() {
     const seen = await AsyncStorage.getItem(STORAGE_KEYS.HAS_SEEN_ONBOARDING);
-    return seen === "true";
+    return seen === "true"
 }
 
 export async function setHasSeenOnboarding() {
     await AsyncStorage.setItem(STORAGE_KEYS.HAS_SEEN_ONBOARDING, "true");
 }
-
+ 
 export async function resetOnboardingForTest() {
     await AsyncStorage.removeItem(STORAGE_KEYS.HAS_SEEN_ONBOARDING)
 }
